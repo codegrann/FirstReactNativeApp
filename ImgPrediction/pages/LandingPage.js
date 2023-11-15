22af7fec2dd3f1b65e06d86c8cb1bc1de6b8ac01
codegrann <omondihills02@gmail.com>
2023-11-15 15:59:55 +0300

    .

--- ImgPrediction/pages/LandingPage.js
+++ ImgPrediction/pages/LandingPage.js
@@ -1,13 +1,25 @@
 // pages/LandingPage.js
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Button } from "react-native";
 
 const LandingPage = () => {
   return (
     <View>
       <Text>Hi Coder</Text>
       {/* Add your logo and other content here */}
-      <Image source={require("../assets/logo.pmg")} />
+      <Image
+        source={require("../assets/logo.png")}
+        style={{
+          position: "absolute",
+          top: "6rem",
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <Button>
+        <Text>Start</Text>
+      </Button>
     </View>
   );
 };
